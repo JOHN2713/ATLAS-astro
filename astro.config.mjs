8cb8f2d440f7be79a54d9cf40124b568e8ff35ed
Johny Vilaña <johnyv1305@gmail.com>
2026-01-28 22:30:42 -0500

    fix: remove Cloudflare adapter, use static output only

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
-  adapter: cloudflare({
-    mode: 'directory'
-  }),
   integrations: [react(), tailwind()],
 });
